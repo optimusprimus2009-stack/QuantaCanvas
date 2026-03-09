60163c6be96a1beae99bca92d3fc336214df890c
optimusprimus2009-stack <optimusprimus2009@gmail.com>
2026-03-09 17:24:35 +0530

    Add files via upload

---
+++ results/script.js
@@ -0,0 +1,43 @@
+var theme = localStorage.getItem("theme")
+
+var data = localStorage.getItem("prompt")
+
+document.getElementById("iframe").srcdoc = data
+
+if (theme == "light") {
+
+    // alert(theme)
+    document.getElementById("icon").href = "icon.ico"
+    document.body.style.backgroundImage = "linear-gradient(90deg, #ccd0da 1px, transparent 0px), linear-gradient(#ccd0da 1px, transparent 0px)"
+
+} else if (theme == "dark") {
+
+    document.getElementById("icon").href = "icon-dark.ico"
+    document.body.classList.toggle("dark");
+    document.body.style.backgroundImage = "linear-gradient(90deg, #363a4f 1px, transparent 0px), linear-gradient(#363a4f 1px, transparent 0px)"
+
+
+    // alert(theme)
+}
+
+
+// alert(theme)
+function change_theme() {
+
+    theme = localStorage.getItem("theme")
+    if (theme == "light") {
+        // alert(theme)
+
+        localStorage.setItem("theme", "dark")
+        document.getElementById("icon").href = "icon-dark.ico"
+        document.body.style.backgroundImage = "linear-gradient(90deg, #363a4f 1px, transparent 0px), linear-gradient(#363a4f 1px, transparent 0px)"
+    } else if (theme == "dark") {
+
+        document.getElementById("icon").href = "icon.ico"
+        document.body.style.backgroundImage = "linear-gradient(90deg, #ccd0da 1px, transparent 0px), linear-gradient(#ccd0da 1px, transparent 0px)"
+        localStorage.setItem("theme", "light")
+        // alert(theme)
+    }
+    document.body.classList.toggle("dark");
+
+}
