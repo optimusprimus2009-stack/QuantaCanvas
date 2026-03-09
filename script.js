21e09cfedea03d5a0552384c727ee460ca835993
optimusprimus2009-stack <optimusprimus2009@gmail.com>
2026-03-09 19:40:33 +0530

    Update script.js

--- script.js
+++ script.js
@@ -57,7 +57,9 @@ input.addEventListener("keypress", function(event) {
 function send_prompt(){
 
     localStorage.setItem("prompt", document.getElementById("promptbar").value)
-  
+  const result = await window.pywebview.api.prompt("hello");
+    alert(result);
 }
+
 
 
